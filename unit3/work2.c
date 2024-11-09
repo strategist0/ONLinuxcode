b89b17bdfdcef968f5b46db4efe31309eda00d6f
strateg1st <3141270268@qq.com>
2024-11-09 20:40:41 +0800

    work

---
+++ unit3/work2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("Enter an ASCII character;");
+    scanf("%d", &a);
+    printf("your ASCII character is %d, char is %c.\n", a, a);
+    return 0;
+}
