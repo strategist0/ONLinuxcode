4a15c8af4bcb593697c4b26ca7f35ad40e8c49c1
strateg1st <3141270268@qq.com>
2024-11-09 20:45:19 +0800

    000

---
+++ unit3/work3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    char ch = '\a';
+    printf("%c", ch);
+    printf("starled by the sudden sound, Sally shouted, \n");
+    printf("\"By the Great Pumkin, waht was that!\"、n");
+    return 0;
+}
