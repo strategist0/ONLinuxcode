ef6d21d865c651af96ad9c69168672ec0d285400
Zhiyuan <3141270268@qq.com>
2024-11-19 21:54:54 +0800

    更新 work.c
    improve

--- work/unit10/work.c
+++ work/unit10/work.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
-#defineMONTHS12//numberofmonthsinayear
-#defineYRS5//numberofyearsofdata
-intmain(void)
+#define MONTHS 12
+#define YRS 5
+int main(void)
 {
-constfloatrain[YRS][MONTHS]={
+const float rain[YRS][MONTHS]={
 {4.3,4.3,4.3,3.0,2.0,1.2,0.2,0.2,0.4,2.4,3.5,6.6},
 {8.5,8.2,1.2,1.6,2.4,0.0,5.2,0.9,0.3,0.9,1.4,7.3},
 {9.1,8.5,6.7,4.3,2.1,0.8,0.2,0.2,1.1,2.3,6.1,8.4},
 {7.2,9.9,8.4,3.3,1.2,0.8,0.4,0.0,0.6,1.7,4.3,6.2},
 {7.6,5.6,3.8,2.8,3.8,0.2,0.0,0.0,0.0,1.3,2.6,5.2}
 };
-intyear,month;
-floatsubtot,total;
+int year,month;
+float subtot,total;
 printf("YEARRAINFALL(inches)\n");
 for(year=0,total=0;year<YRS;year++)
 {
